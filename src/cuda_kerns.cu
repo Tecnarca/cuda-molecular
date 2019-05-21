e9eae8b56197ec833f660faedc8053cfc9c4c517
Marco Fringuelli <marco.fringuelli23@gmail.com>
2019-05-21 21:58:48 +0200

    Update cuda_kerns.cu

--- src/cuda_kerns.cu
+++ src/cuda_kerns.cu
@@ -1,6 +1,7 @@
 #ifndef ARRSIZE
 #define ARRSIZE 256*512*512 
 #endif
+
 #ifndef DEBUG
 #define DEBUG 1
 #define NATOM 4
@@ -8,43 +9,40 @@
 #define VOLUMESIZE 1000000
 #endif
 
+
 extern "C" void ps_kern(float* atoms_in, float* atoms_out, int precision,float* score_pos, int* start, int* stop, int* mask )
 {
 	float *d_atoms_in, *d_score_pos;
 	
-	//do we need these in the gpu?
+	//do we need these in the gpu? yes
 	int* d_start, int* d_stop, int* d_mask;
-	
-	//we do not need atoms_out and precision on the gpu
+
 
 	cudaError_t status;
 
-	status = cudaMalloc((void**) &d_atoms_in, siezof(float)*ARRSIZE);
+	status = cudaMalloc((void**) &d_atoms_in, sizeof(float)*ARRSIZE);
 	
 	if(DEBUG && status!=cudaSuccess) 
 		cout << cudaGetErrorString(status) << " in " << __FILE__ << " at line " << __LINE__ << endl;
 
-	status = cudaMalloc((void**) &d_score_pos, siezof(float)*VOLUMESIZE);
+	status = cudaMalloc((void**) &d_score_pos, sizeof(float)*VOLUMESIZE);
 	
 	if(DEBUG && status!=cudaSuccess) 
 		cout << cudaGetErrorString(status) << " in " << __FILE__ << " at line " << __LINE__ << endl;
 
-	status = cudaMalloc((void**) &d_start, siezof(float)*NATOM);
+	status = cudaMalloc((void**) &d_start, sizeof(float)*NATOM);
 	
 	if(DEBUG && status!=cudaSuccess) 
 		cout << cudaGetErrorString(status) << " in " << __FILE__ << " at line " << __LINE__ << endl;
 
-	status = cudaMalloc((void**) &d_stop, siezof(float)*NATOM);
+	status = cudaMalloc((void**) &d_stop, sizeof(float)*NATOM);
 	
 	if(DEBUG && status!=cudaSuccess) 
 		cout << cudaGetErrorString(status) << " in " << __FILE__ << " at line " << __LINE__ << endl;
 
-	status = cudaMalloc((void**) &d_mask, siezof(float)*MASKSIZE);
+	status = cudaMalloc((void**) &d_mask, sizeof(float)*MASKSIZE);
 	
 	if(DEBUG && status!=cudaSuccess) 
 		cout << cudaGetErrorString(status) << " in " << __FILE__ << " at line " << __LINE__ << endl;
-
-	
-
 
 }
