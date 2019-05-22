dc919a1cf1aa785c2c4dcb737d2f7f3cbe0e05b8
Marco Fringuelli <marco.fringuelli23@gmail.com>
2019-05-22 22:12:44 +0200

    Update measure_shotgun.cu

--- src/measure_shotgun.cu
+++ src/measure_shotgun.cu
@@ -1,7 +1,7 @@
 // I think pocket should be texturizable
 
 __device__ int measure_shotgun (float* atoms, float* pocket)
-	{
+{
     unsigned int threadsPerBlock = blockDim.x;
 
     // one entry per atom processed within block (don't know if it's actually faster)
@@ -9,14 +9,14 @@ __device__ int measure_shotgun (float* atoms, float* pocket)
 
     // each thread will process one atom
     int x = threadId.x;
-		int y = threadId.x + blockDim.x;
-		int z = threadId.x + 2*blockDim.x;
+    int y = threadId.x + blockDim.x;
+    int z = threadId.x + 2*blockDim.x;
 
     unsigned int cacheIndex = x;
     float blockScore[gridDim.x] // one entry per block
 
-		// get the average score
-		int score = 0;
+    // get the average score
+    int score = 0;
 
     int index_x = atoms[x]  * grid_factor_d;
     int index_y = atoms[y]  * grid_factor_d;
@@ -48,31 +48,10 @@ __device__ int measure_shotgun (float* atoms, float* pocket)
       blockScore[BlockId.x] = cache[cacheIndex];
     }
 
-    __syncthreads(); // probably unnecessary
+    __syncthreads(); // probably unnecessary, or probably should be __threadfence_block()
 
     // total score
     return blockScore[0] + blockScore[1] + blockScore[2];
-  }
 
-
-
-
-
-		// loop over the atoms and get the pocket score
-		for ( int i =0; i < n_atoms; ++i )
-		{
-			// compute the index inside of the pocket
-			int index_x = static_cast<int>(atoms[i]  * grid_factor_d );
-			int index_y = static_cast<int>(atoms[i+n_atoms]  * grid_factor_d );
-			int index_z = static_cast<int>(atoms[i+2*n_atoms]  * grid_factor_d );
-			if (index_x < 0) index_x = 0;
-			if (index_x > 100) index_x = 100;
-			if (index_y < 0) index_y = 0;
-			if (index_y > 100) index_y = 100;
-			if (index_z < 0) index_z = 0;
-			if (index_z > 100) index_z = 100;
-			// update the score value
-			score += pocket[index_x+100*index_y+10000*index_z];
-		}
-		return score;
-	}
+    
+}
