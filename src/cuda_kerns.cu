2443b7b7c3dbb817277ea012f2cef5cfded477b6
Tecnarca <mail.tecnarca@gmail.com>
2019-06-04 00:53:50 +0200

    Debugging stage

--- src/cuda_kerns.cu
+++ src/cuda_kerns.cu
@@ -1,5 +1,5 @@
 #ifndef DEBUG
-#define INSIZE 256 //real (used by the main.cpp) dimension of 'in'
+#define INSIZE 192 //real (used by the main.cpp) dimension of 'in'
 #define N_ATOMS 64 //atoms inside 'in'
 #define N_FRAGS 4 //number of fragments
 #define DEBUG 1 //should we execute the debug prints and checks?
@@ -23,8 +23,8 @@ texture<int> texMask;
 
 __device__ void compute_matrix( const float rotation_angle,
                 const float x_orig, const float y_orig, const float z_orig,
-                const float x_vector, const float y_vector, const float z_vector, float* matrix)
-        {
+                const float x_vector, const float y_vector, const float z_vector, float* matrix){
+            
             // compute the rotation axis
             const float u = (float)x_vector - x_orig;
             const float v = (float)y_vector - y_orig;
@@ -70,29 +70,28 @@ __global__ void rotate(float* in, int* mask, float angle, float* in_r, int iter,
 	int x = threadIdx.x;
 	int y = threadIdx.x + blockDim.x;
 	int z = threadIdx.x + 2*blockDim.x;
-
 	float m[12];
 	compute_matrix(angle*precision,in[curr_start],in[curr_start+N_ATOMS],in[curr_start+2*N_ATOMS],in[curr_stop],in[curr_stop+N_ATOMS], in[curr_stop+2*N_ATOMS], m);
-	  
+	 
 	int mask_x = tex1Dfetch(texMask, x+iter*N_ATOMS);
-	if(mask_x == 1){
+	 if(mask_x == 1){
 		// gets current coordinates
 		const float prev_x = in[x];
 		const float prev_y = in[y];
 		const float prev_z = in[z];
 	    // update in (the one that is allocated in the GPU)
-		in_r[x] = m[0] * prev_x + m[1] * prev_y + m[2] * prev_z + m[3];
-		in_r[y] = m[4] * prev_x + m[5] * prev_y + m[6] * prev_z + m[7];
-		in_r[z] = m[8] * prev_x + m[9] * prev_y + m[10] * prev_z + m[11];
+		//in_r[x] = m[0] * prev_x + m[1] * prev_y + m[2] * prev_z + m[3]; //268 Errori (di write): fare in_r[x] = 0; ne restituisce altrettanti
+		//in_r[y] = m[4] * prev_x + m[5] * prev_y + m[6] * prev_z + m[7]; //204 Errori (di write): fare in_r[y] = 0; ne restituisce altrettanti
+		//in_r[z] = m[8] * prev_x + m[9] * prev_y + m[10] * prev_z + m[11]; //268 Errori (di write): fare in_r[x] = 0; ne restituisce altrettanti
 	}
 }
 
-
+//aggiustare in un blocco solo
 __global__ void measure_shotgun (float* atoms, float* pocket, float* scores, int index)
 {
 
     // one entry per atom processed within block (don't know if it's actually faster)
-    __shared__ float cache[THREADSPERBLOCK];
+    __shared__ float cache[INSIZE];
 
     // each thread will process one atom
     int x = threadIdx.x;
@@ -100,11 +99,10 @@ __global__ void measure_shotgun (float* atoms, float* pocket, float* scores, int
     int z = threadIdx.x + 2*blockDim.x;
 
     unsigned int cacheIndex = x;
-    float blockScore[BLOCKS]; // one entry per block
 
-    int index_x = atoms[x]  * GRID_FACTOR_D;
-    int index_y = atoms[y]  * GRID_FACTOR_D;
-    int index_z = atoms[z]  * GRID_FACTOR_D;
+    int index_x = (int) (atoms[x]  * GRID_FACTOR_D);
+    int index_y = (int) (atoms[y]  * GRID_FACTOR_D);
+    int index_z = (int) (atoms[z]  * GRID_FACTOR_D);
 
     if (index_x < 0) index_x = 0;
     if (index_x > 100) index_x = 100;
@@ -114,7 +112,7 @@ __global__ void measure_shotgun (float* atoms, float* pocket, float* scores, int
     if (index_z > 100) index_z = 100;
 
     // perform reduction (compute partial block score)
-    cache[cacheIndex] = tex1Dfetch(texScore_pos, index_x+100*index_y+10000*index_z);
+    //cache[cacheIndex] = tex1Dfetch(texScore_pos, index_x+100*index_y+10000*index_z); //ERROR!
 
     __syncthreads();
 
@@ -128,35 +126,34 @@ __global__ void measure_shotgun (float* atoms, float* pocket, float* scores, int
       i /= 2;
     }
 
-    if(cacheIndex == 0) {
-      blockScore[blockIdx.x] = cache[cacheIndex];
-    }
-
     __syncthreads(); // probably unnecessary, or probably should be __threadfence_block()
 
-    // total score
-    scores[index] = blockScore[0] + blockScore[1] + blockScore[2];
+    if(cacheIndex == 0) {
+      if(index<0 || index > THREADSPERBLOCK-1)printf("(i: %d) ", index); //0 printfs
+      scores[index] = cache[0];
+      atoms[index] = 0;
+    }
 }
 
 
-//I pass is_bumping for thread from each block to access it
-__global__ void fragment_is_bumping(const float* in, const int* mask, bool* cache_is_bumping, int res){
+//THREAD NUMBER INCORRECT: INCAPSULATE
+__global__ void fragment_is_bumping(float* in, const int* mask, bool* cache_is_bumping, int res){
 
 	__shared__ bool all_bumps[INSIZE*(INSIZE-1)/2];
 	//spawn n_atoms threads per block and n_atoms blocks
-	const int ix = threadIdx.x; 
-	const int jx = threadIdx.y; 
-	const int iy = threadIdx.x + blockDim.x; 
-	const int jy = threadIdx.y + blockDim.x; 
-	const int iz = threadIdx.x + 2*blockDim.x; 
-	const int jz = threadIdx.y + 2*blockDim.x;
+	int ix = threadIdx.x; 
+	int jx = threadIdx.y; 
+	int iy = threadIdx.x + blockDim.x; 
+	int jy = threadIdx.y + blockDim.x; 
+	int iz = threadIdx.x + 2*blockDim.x; 
+	int jz = threadIdx.y + 2*blockDim.x;
 	//Unique sequential index for threads with jx>ix
-	const int cacheIndex = ix*(INSIZE-1)-ix*(ix-1)+(ix-1)*ix/2+jx-ix-1;
+	int cacheIndex = ix*(INSIZE-1)-ix*(ix-1)+(ix-1)*ix/2+jx-ix-1;
 	
 	if(jx>ix){
-		int m_ix = tex1Dfetch(texMask, ix);
-		int m_jx = tex1Dfetch(texMask, jx);
-		if(fabsf(m_ix - m_jx) == 1){
+		//int m_ix = tex1Dfetch(texMask, ix);
+		//int m_jx = tex1Dfetch(texMask, jx);
+		//if(fabsf(m_ix - m_jx) == 1){
 
 			const float diff_x = in[ix] - in[jx];
 	        const float diff_y = in[iy] - in[jy];
@@ -177,12 +174,11 @@ __global__ void fragment_is_bumping(const float* in, const int* mask, bool* cach
 	        		__syncthreads();
 	      		}
       			i /= 2;
-    		} //del
+    		}
 
     		if(cacheIndex == 0) {
-      		cache_is_bumping[res] = all_bumps[cacheIndex];
+      			//cache_is_bumping[res] = all_bumps[cacheIndex];
     		}
-		}
 	}
 }
 
@@ -199,21 +195,24 @@ __global__ void eval_angles(float* in, float* score_pos, int curr_start, int cur
 	__shared__ int best_angles[THREADSPERBLOCK];
 
 	unsigned int cacheIndex = angle;
+	
+
+
 	dim3 bumpdim(INSIZE,INSIZE,1);
       
-      //if(DEBUG) printf("(%d: -)\n", angle);
-
-	  rotate<<<BLOCKS,THREADSPERBLOCK>>>(in, mask, angle, &in[angle*MAX_ANGLE], iter, precision, curr_start, curr_stop);
+	//printf("%f",in[angle*N_ATOMS]);
 
 	  best_angles[cacheIndex] = angle;
 
 	  cache_is_bumping[angle]=0;
 
-	  measure_shotgun<<<3, ceilf(N_ATOMS/3) >>>(&in[angle*MAX_ANGLE], score_pos, cache_score, angle);  // populates the scores cache
+	  rotate<<<BLOCKS,THREADSPERBLOCK>>>(in, mask, angle, &in[angle*N_ATOMS], iter, precision, curr_start, curr_stop);
+
+	  measure_shotgun<<<1, INSIZE >>>(&in[angle*N_ATOMS], score_pos, cache_score, angle);  // populates the scores cache
 
 	  //if(DEBUG) printf("score is: %d for fragm %d with angle %f\n", cache_score, angle, angle*precision);
 	  
-	  fragment_is_bumping<<<BLOCKS,bumpdim>>>(&in[angle*MAX_ANGLE], mask, cache_is_bumping, angle); // populates the is_bumping cache
+	  //fragment_is_bumping<<<1,bumpdim>>>(&in[angle*N_ATOMS], mask, cache_is_bumping, angle); // populates the is_bumping cache
 
 		// doubt: I don't know if I can pass shared caches to other nested kernels in total tranquility (I think so).
 		// If not, we need a "buffer" array for the function that will be flushed into the cache (not so bad)
@@ -255,11 +254,12 @@ void ps_kern(float* in, float* out, float precision, float* score_pos, int* star
 
 	//GPU MEMORY INITIALIZATION
 	
-	status = cudaMalloc((void**) &d_in, sizeof(float)*INSIZE*MAX_ANGLE/precision);
+	status = cudaMalloc((void**) &d_in, sizeof(float)*INSIZE*MAX_ANGLE);
 	if(DEBUG && status!=cudaSuccess)
 		printf("%s in %s at line %d\n", cudaGetErrorString(status), __FILE__, __LINE__);
 
-	status_cp = cudaMemcpy(d_in, in, sizeof(float)*INSIZE, cudaMemcpyHostToDevice);
+	//status_cp = cudaMemcpy(d_in, in, sizeof(float)*INSIZE, cudaMemcpyHostToDevice);
+	cudaMemset(d_in, 0.0, sizeof(float)*INSIZE*MAX_ANGLE);
 	if(DEBUG && status_cp!=cudaSuccess)
 		printf("%s in %s at line %d\n", cudaGetErrorString(status_cp), __FILE__, __LINE__);
 
@@ -296,19 +296,11 @@ void ps_kern(float* in, float* out, float precision, float* score_pos, int* star
 	if(DEBUG && status_tx!=cudaSuccess)
 		printf("%s in %s at line %d\n", cudaGetErrorString(status_tx), __FILE__, __LINE__);				
 
-
-	//CUDA stream creation
-	//https://developer.download.nvidia.com/CUDA/training/StreamsAndConcurrencyWebinar.pdf
-	//The webminar above explains how to use effectively the cuda streams
-	cudaStream_t stream1, stream2;
-	cudaStreamCreate(&stream1);
-	cudaStreamCreate(&stream2);
-
 	// start CUDA timing here
 	for(int i=0; i<N_FRAGS; i++){
-		if(DEBUG) printf("ITER: %d\n", i);
-		eval_angles<<<BLOCKS,ceilf(THREADSPERBLOCK/precision)>>>(d_in, d_score_pos, start[i], stop[i], d_mask, precision, i);
-		if(DEBUG) cudaDeviceSynchronize();
+		//if(DEBUG) printf("ITER: %d\n", i);
+		eval_angles<<<BLOCKS,MAX_ANGLE>>>(d_in, d_score_pos, start[i], stop[i], d_mask, precision, i);
+		//if(DEBUG) cudaDeviceSynchronize();
 	}
 	cudaDeviceSynchronize();
 	//stop CUDA timing here
